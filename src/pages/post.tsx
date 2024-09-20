4bfb6d7f79c3727908b53bbffc402852afd445c1
PedroGaya <pedrohenriquegaya@gmail.com>
2024-09-20 13:28:25 -0300

    props for posts

--- src/pages/post.tsx
+++ src/pages/post.tsx
@@ -5,22 +5,8 @@ import ReactMarkdown from "react-markdown";
 import rehypeRaw from "rehype-raw";
 import remarkGfm from "remark-gfm";
 
-export default function BlogPost() {
-  const md = `
-        Hello this **video** is embeded from Youtube!
-    
-        <div style="display: flex; justify-content: center;">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/JQvc-Gkwhow?si=eFOjL6lGKQXFZmHJ" title="My Favorite Things" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        </div>
-    
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tincidunt mattis orci non fermentum. Donec id erat eleifend, tincidunt ante sed, bibendum massa. Fusce a sodales dui, aliquet condimentum mauris. Vestibulum euismod tristique condimentum. Proin eget placerat tortor, vel maximus lectus. Praesent leo sem, varius non felis at, ultricies bibendum ex. Integer eu elementum elit, sed blandit justo. Suspendisse potenti. Proin nisi ligula, tincidunt dictum risus rhoncus, dapibus aliquet ante. Praesent fringilla cursus vulputate. Curabitur facilisis arcu at augue gravida, vel fringilla felis eleifend. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aenean nec magna non orci ornare dignissim. Sed vel pulvinar sem.
-        
-        \`\`\`js
-        function test() {
-            console.log("notice the blank line before this function?");
-        }
-        \`\`\`
-        `;
+export default function BlogPost(props: { content: string }) {
+  const { content } = props;
 
   return (
     <DefaultLayout>
@@ -28,7 +14,7 @@ export default function BlogPost() {
         <div className="inline-block max-w-lg text-center justify-center">
           <h1 className={title()}>Post Name</h1>
         </div>
-        <Markdown content={md} />
+        <Markdown content={content} />
       </section>
     </DefaultLayout>
   );
