0bf11b0daf34a0e3bcc29c2bfe9f3785489c92e6
PedroGaya <pedrohenriquegaya@gmail.com>
2024-09-21 13:04:46 -0300

    force overflow in layout to prevent flex errors

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -7,7 +7,7 @@ export default function DefaultLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="relative flex flex-col h-screen">
+    <div className="relative flex flex-col h-screen overflow-y-scroll">
       <Navbar />
       <main className="container mx-auto max-w-7xl px-6 flex-grow pt-16">
         {children}
@@ -15,7 +15,9 @@ export default function DefaultLayout({
       <Divider />
       <footer className="w-full flex items-center justify-center py-3">
         <div className="flex items-center gap-1 text-current">
-          <span className="text-default-600">Made and copyrighted by me</span>
+          <span className="text-default-600">
+            Made and copyrighted by me | pedrohenriquegaya@gmail.com
+          </span>
         </div>
       </footer>
     </div>
